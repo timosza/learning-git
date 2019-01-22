603cf02f1442f79bb0d302c5947048e0b8dff271
Timosza <timmymail@yandex.ru>
2019-01-22 15:18:03 +0700

    Added JS code

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
+window.onload = function() {
+    
+    var sign = document.getElementById('scratch_cat');
+    
+    sign.style.color = 'red';
 
+    alert (sign.style.color == 'red' ? 'It\'s red' : 'It\'s not red');
+}
